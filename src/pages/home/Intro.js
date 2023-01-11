02e65cc0c6176770f22afdd5dd34cffd9b5be540
ecaiyonotbad <ccyedward@me.com>
2023-01-10 21:44:57 -0800

    Intro.js done

--- src/pages/home/Intro.js
+++ src/pages/home/Intro.js
@@ -1,39 +1,30 @@
-import {Container, Grid, Typography} from "@mui/material";
+import {Container, Fade, Grid, Typography} from "@mui/material";
 
 export default function Intro() {
     return (
         <div>
-            <Container sx={{marginTop: "15vh"}}>
+            <Container sx={{marginTop: "35vh", marginBottom: "35vh"}}>
                 <Grid container>
                     <Grid item xs={8}>
+                        <Fade in={true} timeout={5000}>
 
-                        <Grid item xs={12}>
-                            <Typography component="h1" variant="h3" align="left" color="textPrimary" gutterBottom>
-                                Hi! My name is
-                            </Typography>
-                            <Typography component="h1" variant="h3" align="left" color="textPrimary" gutterBottom
-                                        sx={{color: "#66FCF1"}}>
-                                Edward Chen
-                            </Typography>
-                            <Typography component="h1" variant="h5" align="left" color="textPrimary" gutterBottom>
-                                I am a fifth year Computer Science student at The University of British Columbia,
-                                aspiring to become a Cloud Architect.
-                            </Typography>
+                            <Grid item xs={12}>
+                                <Typography component="h1" variant="h3" align="left" color="textPrimary" gutterBottom>
+                                    Hi! My name is
+                                </Typography>
 
-                        </Grid>
-                        {/*<Grid item xs={12}>*/}
-                        {/*</Grid>*/}
-                        {/*<Grid item xs={10}>*/}
-                        {/*    <Typography component="h6" variant="h6" align="left" color="textPrimary" gutterBottom>*/}
-                        {/*        I am currently a fifth year Computer Science student at The University of British*/}
-                        {/*        Columbia. I have a strong interest in cloud computing and software development,*/}
-                        {/*        and I am excited to begin exploring career opportunities in these fields.*/}
-                        {/*    </Typography>*/}
-                        {/*</Grid>*/}
+                                <Typography component="h1" variant="h3" align="left" color="textPrimary" gutterBottom
+                                            sx={{color: "#66FCF1"}}>
+                                    Edward Chen
+                                </Typography>
+
+                                <Typography component="h1" variant="h5" align="left" color="textPrimary" gutterBottom>
+                                    I am a fifth year Computer Science student at The University of British Columbia,
+                                    aspiring to become a Cloud Architect.
+                                </Typography>
+                            </Grid>
+                        </Fade>
                     </Grid>
-                    {/*<Grid item xs={4}>*/}
-                    {/*    <CardMedia sx={{height: "50vh", borderRadius: "15px"}} image={goKart}/>*/}
-                    {/*</Grid>*/}
                 </Grid>
             </Container>
 
