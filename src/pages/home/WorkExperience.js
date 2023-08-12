519602e8295180d32e8c979a491411cb0443cba3
Edward Chen <cyyedward@hotmail.com>
2023-08-12 13:10:57 -0700

    work exp update

--- src/pages/home/WorkExperience.js
+++ src/pages/home/WorkExperience.js
@@ -17,7 +17,7 @@ export default function WorkExperience() {
                 <Grid container spacing={3}>
                     <Grid item xs={12}>
                         <WorkCard image={awsLogo} company={"Amazon Web Services"}
-                                  jobTitle={"Professional Services Cloud Consultant"} date={"Current"}
+                                  jobTitle={"Professional Services Cloud Consultant"} date={"Jul 2023 - Current"}
                                   contents={[]}
                                   tags={[]}
                                   companyLink={"https://aws.amazon.com/"}/>
